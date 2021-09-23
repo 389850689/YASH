7317d9549c48199856f5a225257dd168ea7ab975
(λx.xx)(λx.xx) <49432296+389850689@users.noreply.github.com>
2021-09-22 18:35:25 -0700

    added dbg print

--- src/driver/lib.rs
+++ src/driver/lib.rs
@@ -1,13 +1,14 @@
 #![no_std]
 
+/// causes linker errors when using floating point instructions 
+/// workaround: https://github.com/rust-lang/rust/issues/62785/
+#[used]
+#[no_mangle]
+pub static _fltused: i32 = 0;
+
 mod log;
 
 use core::panic::PanicInfo;
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
 
 #[no_mangle]
 pub extern "system" fn driver_entry() -> u32 {
@@ -15,4 +16,9 @@ pub extern "system" fn driver_entry() -> u32 {
     dbg_print!("test");
 
     0 /* STATUS_SUCCESS */
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
--- src/driver/log.rs
+++ src/driver/log.rs
@@ -1,14 +1,48 @@
+///!
+///! NOTE: I might completely remove this implementation of printing (implementing fmt::Write)
+///! and instead create a wrapper for kernel heap allocations for collections
+///! 
+
+use core::fmt::{self, Write};
 pub use winapi::km::wdm::DbgPrint;
 
 /// Sends a message to the kernel debugger.
 /// 
 /// This uses rust's default string formatting style.
 #[macro_export]
-macro_rules! print {
+macro_rules! dbg_print {
     ($($arg:tt)*) => ($crate::log::_print(format_args!($($arg)*)));
 }
 
+/// Wrapper struct to provide an API for writing a string to an array.
+/// More importantly, after we implement the fmt::Write trait we can 
+/// use rust's format strings on byte arrays.
 #[doc(hidden)]
-fn _print(args: core::fmt::Arguments) {
+struct Wrapper<'a> {
+    buf: &'a mut [u8],
+}
+
+impl<'a> Wrapper<'a> {
+    fn new(buf: &'a mut [u8]) -> Self { Self { buf } }
+}
+
+impl fmt::Write for Wrapper<'_>
+{
+    fn write_str(&mut self, s: &str) -> Result<(), fmt::Error> {
+        let chars = &s.as_bytes()[..s.len() - 1];
+        self.buf.copy_from_slice(chars);
+        Ok(())
+    }
+}
+
+#[doc(hidden)]
+pub fn _print(args: core::fmt::Arguments) {
+    // buffer to place string in, minus 1, for the null term
+    let mut buffer: [u8; 0x1000] = [0; 0x1000];
     
+    // write format arguments string to buffer
+    Wrapper::new(&mut buffer).write_fmt(args).unwrap();
+
+    // NOTE: I believe this is considered bad practice, even though it really isn't unsafe.
+    unsafe { DbgPrint(&buffer as *const u8); }
 }
