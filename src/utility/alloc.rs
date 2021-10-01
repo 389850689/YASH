b237e887745cd7f244cbb53e99fdc1c02ee3a686
(λx.xx)(λx.xx) <49432296+389850689@users.noreply.github.com>
2021-10-01 10:30:27 -0700

    I'm honestly so pissed at this point it's actually crazy to me how doing this one simple ass thing can cause so many problems like I simply just want to turn a 4 byte char array into a DWORD like this should be the least of my problems

--- src/utility/alloc.rs
+++ src/utility/alloc.rs
@@ -4,40 +4,41 @@ use core::alloc::{GlobalAlloc, Layout};
 use lazy_static::lazy_static;
 use crate::include::interop::Allocate::*;
 
+#[global_allocator]
+static GLOBAL: AllocatePool = AllocatePool { tag: None, tag_created: false };
+
 struct AllocatePool {
-    tag: u32
+    tag: Option<u32>,
+    tag_created: bool
 }
 
 impl AllocatePool {
-    const fn new(tag: &'static str) -> Self {
-        Self {
+    fn create_tag(&mut self, tag: &'static str) {
             /// Turns string into u32.
             /// 
             /// Uses u32 function `from_le_bytes` to turn a 4 byte array of u8 values
             /// into a single u32 value; to accomplish this we turn the string into a
             /// slice of bytes and try_into to convert it according to the argument type.
-            #[feature(const_panic)]
-            tag: u32::from_le_bytes(tag.as_bytes().try_into().unwrap())
+            self.tag = Some(u32::from_le_bytes(tag.as_bytes().try_into().unwrap()));
+            self.tag_created = true;
         }
     }
 }
 
 unsafe impl GlobalAlloc for AllocatePool {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        // NOTE: this is very questionable, and is subject to change.
+
+
         let allocation = ExAllocatePool2(POOL_FLAGS::POOL_FLAG_PAGED as u64, 
             layout.size(), self.tag);
         
         if allocation.is_null() { panic!() }
         
-        allocation as _
+    
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
         
     }
-}
-
-lazy_static! {
-    //#[global_allocator]
-    static ref GLOBAL: AllocatePool = AllocatePool::new("Test");
 }
